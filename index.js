d59112be05c2378d80a748b359ec66bde2abce3f
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2022-01-05 18:38:19 +0100

    a

---
+++ index.js
@@ -0,0 +1,18 @@
+// import express from 'express';
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function (err, data) {
+      console.log(data);
+      res.end(data);
+   });
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
